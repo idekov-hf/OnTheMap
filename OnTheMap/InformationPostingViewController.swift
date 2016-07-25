be96ef2c647300b3d1dc37882e9f13b87e0354f4
Iavor Dekov <ivdekov@gmail.com>
2016-07-25 16:56:44 -0400

    Add InformationPostingViewController and Cancel button with associated Action method in UIViewController subclass

---
+++ OnTheMap/InformationPostingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InformationPostingViewController.swift
+//  OnTheMap
+//
+//  Created by Iavor V. Dekov on 7/25/16.
+//  Copyright © 2016 Iavor Dekov. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - InformationPostingViewController
+
+class InformationPostingViewController: UIViewController {
+
+	@IBAction func cancelButtonPressed(sender: UIButton) {
+		
+		dismissViewControllerAnimated(true, completion: nil)
+	}
+	
+}
