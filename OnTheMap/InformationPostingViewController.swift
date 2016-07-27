c2a89be883381c055a0c6cf27c8b90a4f52bf9b1
Iavor Dekov <ivdekov@gmail.com>
2016-07-26 23:21:31 -0400

    Add to Information Posting Controller

--- OnTheMap/InformationPostingViewController.swift
+++ OnTheMap/InformationPostingViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import MapKit
 
-// MARK: - InformationPostingViewController
+// MARK: - Information Posting View Controller
 
 class InformationPostingViewController: UIViewController {
-
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var linkTextField: UITextField!
+    
+    // MARK: Actions
+    
 	@IBAction func cancelButtonPressed(sender: UIButton) {
 		
 		dismissViewControllerAnimated(true, completion: nil)
 	}
 	
+    @IBAction func findOnMapButtonPressed(sender: UIButton) {
+        
+        firstView.hidden = true
+        
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(locationTextField.text!) { (placemark, error) in
+            if let error = error {
+                
+                print(error)
+                
+            } else {
+                
+                print(placemark![0].location?.coordinate)
+                let coordinate = placemark![0].location?.coordinate
+                
+                var annotations = [MKPointAnnotation]()
+                let annotation = MKPointAnnotation()
+                annotation.coordinate = coordinate!
+                annotations.append(annotation)
+                
+                self.mapView.showAnnotations(annotations, animated: true)
+            }
+        }
+    }
 }
